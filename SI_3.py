327b46be5dc31a2fdc5ebd5dec80f0c515998b8f
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-10-18 16:45:10 +0530

    Update SI_3.py

--- SI_3.py
+++ SI_3.py
@@ -4,16 +4,16 @@ Spyder Editor
 This is a temporary script file.
 """
 
-#getting imprtant libraries
+#getting important libraries
 import pygame, sys
 
 pygame.init()
 clock=pygame.time.Clock()
 
-#creating the screen where everting will be displayed
+#creating the screen where everyting will be displayed.
 screen = pygame.display.set_mode((400,600))
 
-#creating objects of game (player, emeny ships)
+#creating objects of the game (player, emeny ships)
 player=pygame.Rect(200,500,30,30)
 playerSpeed=20
 enemy=pygame.Rect(70,50,40,40)
@@ -22,7 +22,7 @@ enemyspeed= -2;
 #game loop
 while True:    
     screen.fill((0,0,0))  #making the screen background black
-    #handling various events such as key press and mouse click
+    #handling various events such as key press and mouse click.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -34,20 +34,20 @@ while True:
             if event.key ==pygame.K_RIGHT:
                 player.x +=playerSpeed
     
-    #maving the enemy at x axis
+    #moving the enemy at x axis
     enemy.x= enemy.x + enemyspeed
     
-    #changing enemyspeed to positive or negative on raching the edge of screen.
-    #making enemy go to right
+    #changing enemyspeed to positive or negative on reaching the edge of the screen.
+    #making enemy go towards the right.
     if enemy.x == 0:
         enemyspeed=enemyspeed * -1
         enemy.y= enemy.y + 20
-    #making enemy go to left
+    #making enemy go towards the left
     if enemy.x == 380:
         enemyspeed=enemyspeed * -1
         enemy.y= enemy.y + 20
     
-    #drawing the characters on screen       
+    #drawing the characters on screen.       
     pygame.draw.rect(screen,(123,200,100),enemy)
     pygame.draw.rect(screen,(23,100,100),player)
     
